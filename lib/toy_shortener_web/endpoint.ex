ecc644241c7524792a415d51f559e5b9bbf130a8
Hendra <hendra2392@gmail.com>
2018-01-17 10:18:40 +0800

    Clean up unused socket code

--- lib/toy_shortener_web/endpoint.ex
+++ lib/toy_shortener_web/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule ToyShortenerWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :toy_shortener
 
-  socket "/socket", ToyShortenerWeb.UserSocket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
